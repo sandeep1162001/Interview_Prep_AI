092d2d2eafe5af5edf5544a1711b1591de7dbfa9
sandeep1162001 <sandeep1162001@gmail.com>
2025-09-03 19:38:53 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(
     origin: process.env.CLIENT_URI,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
