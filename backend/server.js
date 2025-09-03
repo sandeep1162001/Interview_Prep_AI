aa55d45e0ecfd3d1f51865fc34587d532ef707ab
sandeep1162001 <sandeep1162001@gmail.com>
2025-09-03 19:27:24 +0530

    CLIENT_URI

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URI,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
